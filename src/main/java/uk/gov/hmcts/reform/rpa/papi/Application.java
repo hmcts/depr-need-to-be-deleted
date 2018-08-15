f1c3b5b467eb4154680932034304953bab0b94ed
alexis.gayte <alexis.gayte@gmail.com>
2018-08-15 14:04:52 +0100

    Application gradle configuration
    - changed path to papi
    - removed database configuration
    - removed useless dependencies

--- src/main/java/uk/gov/hmcts/reform/rpa/papi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/rpa/papi/Application.java
@@ -1,13 +1,9 @@
-package uk.gov.hmcts.reform.demo;
+package uk.gov.hmcts.reform.rpa.papi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableCircuitBreaker
-@EnableHystrixDashboard
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
