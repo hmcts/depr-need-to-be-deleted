e163221a5ece20618588d13ce9e61b812db657c0
alexis.gayte <alexis.gayte@gmail.com>
2018-08-15 15:59:40 +0100

    add common_tags

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,6 +19,7 @@ module "rpa-professional-api" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
+  common_tags         = "${var.common_tags}"
 
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
