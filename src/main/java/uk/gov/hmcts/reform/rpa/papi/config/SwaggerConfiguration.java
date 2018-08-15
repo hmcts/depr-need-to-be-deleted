e79dda59a6aa0af1bbf4664964077ff50ac95d59
alexis.gayte <alexis.gayte@gmail.com>
2018-08-15 14:17:51 +0100

    fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/rpa/papi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/rpa/papi/config/SwaggerConfiguration.java
@@ -1,14 +1,15 @@
 package uk.gov.hmcts.reform.rpa.papi.config;
 
-import uk.gov.hmcts.reform.rpa.papi.Application;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import uk.gov.hmcts.reform.rpa.papi.Application;
 
 @Configuration
 @EnableSwagger2
