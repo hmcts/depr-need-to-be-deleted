0430c3853f2a8defd825df7dff4b2d4dd9855471
alexis.gayte <alexis.gayte@gmail.com>
2018-08-15 16:04:58 +0100

    remove s2s_url output

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,7 +1,3 @@
 output "microserviceName" {
   value = "${var.component}"
 }
-
-output "s2s_url" {
-  value = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
-}
