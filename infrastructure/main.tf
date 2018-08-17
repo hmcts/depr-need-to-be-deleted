d7c1f54bf2a66b6e54fc521d2b25d90c9332c179
alexis.gayte <alexis.gayte@gmail.com>
2018-08-17 11:48:41 +0100

    Tried the api gateway config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,3 +26,26 @@ module "rpa-professional-api" {
     LOGBACK_REQUIRE_ERROR_CODE  = false
   }
 }
+
+# region API (gateway)
+
+data "template_file" "api_template" {
+  template = "${file("${path.module}/template/api.json")}"
+}
+
+resource "azurerm_template_deployment" "api" {
+  template_body       = "${data.template_file.api_template.rendered}"
+  name                = "${var.product}-api-${var.env}"
+  deployment_mode     = "Incremental"
+  resource_group_name = "core-infra-${var.env}"
+  count               = "${local.create_api ? 1 : 0}"
+
+  parameters = {
+    apiManagementServiceName  = "core-api-mgmt-${var.env}"
+    apiName                   = "professional-api"
+    apiProductName            = "test"
+    serviceUrl                = "http://${var.product}-${local.app}-${var.env}.service.core-compute-${var.env}.internal"
+    apiBasePath               = "${local.api_base_path}"
+    policy                    = "${local.api_policy}"
+  }
+}
