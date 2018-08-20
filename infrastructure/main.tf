bd4a78a65163a5f5ba7114ddb5eda996e6bac193
alexis.gayte <alexis.gayte@gmail.com>
2018-08-20 12:20:56 +0100

    comment source raange

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_subnet" "api-papi-subnet" {
   name                 = "core-infra-subnet-papi-${var.env}"
   resource_group_name  = "${var.vnet_rg_name}"
   virtual_network_name = "${var.vnet_name}"
-  address_prefix       = "${cidrsubnet("${var.source_range}", 4, var.source_range_index)}"
+#  address_prefix       = "${cidrsubnet("${var.source_range}", 4, var.source_range_index)}"
 
   lifecycle {
     ignore_changes = "address_prefix"
