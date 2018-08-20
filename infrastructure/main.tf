53c9dfc2b7e168c0a66579ff07587b4c183c720a
alexis.gayte <alexis.gayte@gmail.com>
2018-08-20 10:58:21 +0100

    tried to create an api management via terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,7 +9,8 @@ resource "azurerm_resource_group" "rg" {
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   
-  app = "papi-gateway"
+  app = "papi-api"
+  
   allowed_certificate_thumbprints = [
     "${var.api_gateway_test_certificate_thumbprint}"
   ]
@@ -37,10 +38,53 @@ module "rpa-professional-api" {
   }
 }
 
-# region API (gateway)
+# region API
+# region API manager
+
+locals {
+  name = "papi-gateway-${var.env}"
+  platform_api_papi_sku = "${var.env == "prod" ? "Premium" : "Developer"}"
+  
+}
+
+data "template_file" "papi_gateway_template" {
+  template = "${file("${path.module}/templates/professional-api-management.json")}"
+}
+
+resource "azurerm_subnet" "api-papi-subnet" {
+  name                 = "core-infra-subnet-papi-${var.env}"
+  resource_group_name  = "${var.vnet_rg_name}"
+  virtual_network_name = "${var.vnet_name}"
+  address_prefix       = "${cidrsubnet("${var.source_range}", 4, var.source_range_index)}"
+
+  lifecycle {
+    ignore_changes = "address_prefix"
+  }
+}
+
+resource "azurerm_template_deployment" "api-managment" {
+  template_body       = "${data.template_file.papi_gateway_template.rendered}"
+  name                = "${local.name}"
+  resource_group_name = "${var.vnet_rg_name}"
+  deployment_mode     = "Incremental"
+
+  parameters = {
+    location                           = "${var.location}"
+    publisher_email                    = "${var.publisher_email}"
+    publisher_name                     = "${var.publisher_name}"
+    notification_sender_email          = "${var.notification_sender_email}"
+    env                                = "${var.env}"
+    platform_papi_name             = "${local.name}"
+    platform_papi_subnetResourceId = "${azurerm_subnet.api-papi-subnet.id}"
+    platform_papi_sku              = "${local.platform_api_papi_sku}"
+  }
+}
+
+
+# region API template 
 
 data "template_file" "api_template" {
-  template = "${file("${path.module}/template/api.json")}"
+  template = "${file("${path.module}/template/api-template.json")}"
 }
 
 resource "azurerm_template_deployment" "api" {
@@ -51,9 +95,9 @@ resource "azurerm_template_deployment" "api" {
   count               = "1"
 
   parameters = {
-    apiManagementServiceName  = "core-api-mgmt-${var.env}"
+    apiManagementServiceName  = "professional-api-${var.env}"
     apiName                   = "professional-api"
-    apiProductName            = "test"
+    apiProductName            = "papi"
     serviceUrl                = "http://${var.product}-${local.app}-${var.env}.service.core-compute-${var.env}.internal"
     apiBasePath               = "${local.api_base_path}"
     policy                    = "${local.api_policy}"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -27,7 +27,50 @@ variable "common_tags" {
   type = "map"
 }
 
+
+
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "source_range" {
+  type = "string"
+}
+
+variable "source_range_index" {}
+
+variable "vnet_rg_name" {
+  type = "string"
+}
+
+variable "vnet_name" {
+  type = "string"
+}
+
+variable "publisher_email" {
+  type    = "string"
+  default = "papi-mangement@hmcts.net"
+}
+
+variable "publisher_name" {
+  type    = "string"
+  default = "HMCTS Reform Platform Professional API"
+}
+
+variable "notification_sender_email" {
+  type    = "string"
+  default = "professional-api-noreply@mail.windowsazure.com"
+}
+
+
+
+
+
 variable api_gateway_test_certificate_thumbprint {
   type = "string"
   default = ""
 }
+
+
