e2d2e684a4cb4e6aad763794772bc097378d8f85
alexis.gayte <alexis.gayte@gmail.com>
2018-08-23 15:40:12 +0100

    change the name again

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,7 +30,7 @@ module "rpa-professional-api" {
 
 module "local_key_vault" {
   source 					= "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                      = "${var.product}-${var.env}"
+  name                      = "${var.product}-papi-${var.env}"
   product 					= "${var.product}-${var.component}"
   env 						= "${var.env}"
   tenant_id 				= "${var.tenant_id}"
