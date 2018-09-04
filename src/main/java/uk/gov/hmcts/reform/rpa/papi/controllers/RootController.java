d87eb25b5bdf0e3c7b5b31a9d3155277e302ed29
alexis.gayte <alexis.gayte@gmail.com>
2018-09-04 12:18:26 +0100

    add log

--- src/main/java/uk/gov/hmcts/reform/rpa/papi/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/rpa/papi/controllers/RootController.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.rpa.papi.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,9 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class RootController {
 
+
+    private static final Logger log = LoggerFactory.getLogger(RootController.class);
+
     /**
      * Root GET endpoint.
      *
@@ -23,16 +28,19 @@ public class RootController {
      */
     @GetMapping("/hello")
     public ResponseEntity<String> welcome() {
+        log.error("is that an welcome error ");
         return ok("Welcome to Professional API");
     }
 
     @GetMapping()
     public ResponseEntity<String> ping() {
+        log.error("is that an ping error ");
         return ok("pong");
     }
 
     @GetMapping("/true")
     public ResponseEntity<Boolean> isTrue() {
+        log.error("is that an true error ");
         return ok(true);
     }
 
