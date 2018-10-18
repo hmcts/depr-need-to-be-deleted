9e647b7854bcad17a270f719b077cdf2060f24b4
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-18 16:27:34 +0100

    Delete starter and ultimate subscriptions after deploy

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,15 @@
 output "microserviceName" {
   value = "${var.component}"
 }
+
+output "subscription-id" {
+  value = "${var.subscription}"
+}
+
+output "resource-group-name" {
+  value = "${azurerm_resource_group.rg.name}"
+}
+
+output "service-name" {
+  value = "${local.apiManagementServiceName}"
+}
