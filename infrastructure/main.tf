5ec6411e1e1caf57e4811416b2322fabdb8a466c
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-25 11:25:56 +0100

    Revert "Add azure vault configuration"
    This reverts commit 84907cffa190964ebe8bbf7cc467e276ba9549a7.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,8 +4,6 @@ locals {
   ase_name  = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   app       = "professional-api"
   apiManagementServiceName  = "rpa-professional-api-portal-${var.env}"
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  shared_vault_name = "${var.shared_product_name}-${local.local_env}"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -77,9 +75,4 @@ resource "azurerm_template_deployment" "api" {
     testDefinitionBody        = "${data.template_file.test_api_def.rendered}"
     policy                    = "${file("template/api-policy.xml")}"
   }
-}
-
-data "azurerm_key_vault" "key_vault" {
-    name = "${local.shared_vault_name}"
-    resource_group_name = "${local.shared_vault_name}"
 }
