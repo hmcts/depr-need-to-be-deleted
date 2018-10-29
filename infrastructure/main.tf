601302688df84ee7957999b58a735dc2c9871e4a
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-29 11:57:16 +0000

    Prevent preview builds using the wrong environment name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,13 +3,13 @@ provider "azurerm" {}
 locals {
   ase_name  = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   app       = "professional-api"
-  apiManagementServiceName  = "rpa-professional-api-portal-${var.env}"
+  apiManagementServiceName  = "rpa-professional-api-portal-${local.local_env}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
   shared_vault_name = "rpa-professional-api-${local.local_env}"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
+  name     = "${var.product}-${var.component}-${local.local_env}"
   location = "${var.location}"
 }
 
@@ -17,14 +17,14 @@ module "rpa-professional-api" {
   source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product             = "${var.product}-${var.component}"
   location            = "${var.location}"
-  env                 = "${var.env}"
+  env                 = "${local.local_env}"
   ilbIp               = "${var.ilbIp}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
   common_tags         = "${var.common_tags}"
-  asp_rg              = "rpa-${var.env}"
-  asp_name            = "rpa-${var.env}"
+  asp_rg              = "rpa-${local.local_env}"
+  asp_name            = "rpa-${local.local_env}"
 
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
@@ -36,7 +36,7 @@ module "local_key_vault" {
   source 					= "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name            = "${local.shared_vault_name}"
   product         = "${var.product}"
-  env 						= "${var.env}"
+  env 						= "${local.local_env}"
   tenant_id 				= "${var.tenant_id}"
   object_id 				= "${var.jenkins_AAD_objectId}"
   resource_group_name 		= "${azurerm_resource_group.rg.name}"
@@ -52,20 +52,20 @@ data "template_file" "api_template" {
 data "template_file" "test_api_def" {
   template = "${file("${path.module}/template/test-api-docs.json")}"
   vars {
-    testServiceUrl = "rpa-professional-api-${var.env}.service.core-compute-${var.env}.internal"
+    testServiceUrl = "rpa-professional-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
   }
 }
 
 data "template_file" "claim_api_def" {
   template = "${file("${path.module}/template/claim-api-docs.json")}"
   vars {
-    claimServiceUrl = "cmc-claim-store-${var.env}.service.core-compute-${var.env}.internal"
+    claimServiceUrl = "cmc-claim-store-${local.local_env}.service.core-compute-${local.local_env}.internal"
   }
 }
 
 resource "azurerm_template_deployment" "api" {
   template_body       = "${data.template_file.api_template.rendered}"
-  name                = "${var.product}-${var.component}-${var.env}"
+  name                = "${var.product}-${var.component}-${local.local_env}"
   deployment_mode     = "Incremental"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   count               = "1"
