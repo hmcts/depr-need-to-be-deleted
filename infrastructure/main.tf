26e5c7114a21347d424ca32807dcf19f67e22312
alexis.gayte <alexis.gayte@gmail.com>
2019-06-18 15:07:54 +0100

    fix issue remove local vault resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,19 +5,12 @@ locals {
   app       = "professional-api"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
-}
-
-
 module "rpa-professional-api" {
   source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product             = "${var.product}-${var.component}"
   location            = "${var.location}"
   env                 = "${var.env}"
   ilbIp               = "${var.ilbIp}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
   common_tags         = "${var.common_tags}"
@@ -30,16 +23,6 @@ module "rpa-professional-api" {
   }
 }
 
-module "local_key_vault" {
-  source 					= "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                      = "${var.product}-papi-${var.env}"
-  product 					= "${var.product}-${var.component}"
-  env 						= "${var.env}"
-  tenant_id 				= "${var.tenant_id}"
-  object_id 				= "${var.jenkins_AAD_objectId}"  
-  resource_group_name 		= "${azurerm_resource_group.rg.name}"
-  product_group_object_id 	= "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
-}
 
 # region API template 
 
